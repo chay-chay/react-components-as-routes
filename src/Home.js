f248a8679ddb3f6ec66722df396e2b814dfbd316
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-03-06 16:22:51 -0500

    Done.

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+// src/Home.js
+
+//Refactor form from const Home = () => <h1>Home!</h1>;
+
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return <h1>Home!</h1>
+  }
+}
+
+export default Home; 
---
+++ src/Messages.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+
+class Messages extends Component{
+    render(){
+        return(
+            <h1> Hello world!</h1>
+        )
+    }
+}
+export default Messages;
